c0229d5b2ede6e8ca5f8d35556e1144864db5045
loriane94 <loriane.ricoux@hotmail.fr>
2015-04-10 10:58:40 +0200

    Initialisation de Git

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+    
+    <?php
+
+    require 'point.php';
+  
+    $mon_point = new point(30,40,"blue");
+    $mon_point->Afficher();
+
+    ?>
+        
+    </body>
+</html>
